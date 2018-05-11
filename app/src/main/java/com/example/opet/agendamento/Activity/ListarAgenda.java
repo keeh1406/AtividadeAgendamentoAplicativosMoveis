b3e1606b1baf3a9c262374f0fd25d8e5b58b8f82
opet <opet@Academico.edu>
2018-05-11 08:46:33 -0300

    11/05/2018 08h47

--- app/src/main/java/com/example/opet/agendamento/Activity/ListarAgenda.java
+++ app/src/main/java/com/example/opet/agendamento/Activity/ListarAgenda.java
@@ -1,8 +1,40 @@
 package com.example.opet.agendamento.Activity;
 
+import android.view.View;
+import android.widget.ListView;
+
+import com.example.opet.agendamento.DAO.AgendaDAO;
+import com.example.opet.agendamento.Model.Agenda;
+
 /**
  * Created by opet on 09/05/2018.
  */
 
 public class ListarAgenda {
+    private ListView listaAgenda;
+    AgendaDAO agendaDAO;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_produtos);
+
+        carregarElementos();
+    }
+
+    public void carregarElementos(){
+        listaAgenda = (ListView) findViewById(R.id.listaAgenda);
+        agendaDAO = new AgendaDAO(this);
+        List<Agenda> agendas = agendaDAO.carregaDadosLista();
+        myAdapter = new AgendaAdapter(this, R.layout.item_produto,produtos);
+        listaAgenda.setAdapter(myAdapter);
+        listaAgenda.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Agenda agenda = (Agenda) parent.getItemAtPosition(position);
+                Intent atualizarIntent = new Intent(ListarProdutosActivity.this,AtualizarAgenda.class);
+                atualizarIntent.putExtra("ID",agenda.getID());
+                startActivity(atualizarIntent);
+            }
+        });
+    }
 }
